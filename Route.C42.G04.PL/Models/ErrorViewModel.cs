4119c04e768ea9fc0421db83fd03707d44b3590d
entsarahmed <entesara500@gmail.com>
2024-09-10 17:56:44 +0300

    Add project files.

---
+++ Route.C42.G04.PL/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Route.C42.G04.PL.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
