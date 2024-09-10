3cb7a20c0306ee162d5a538ba9e1bc3fe8295236
entsarahmed <entesara500@gmail.com>
2024-09-10 21:06:35 +0300

    GitHub - Assignment  (Continued)

---
+++ Route.C42.G04.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Route.C42.G04.DAL.Data.Configurations;
+using Route.C42.G04.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.C42.G04.DAL.Data
+{
+    internal class ApplicationDbContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+       => optionsBuilder.UseSqlServer("Server = .; Database = MVCApplication; Trusted_Connection = True;");
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           // modelBuilder.ApplyConfiguration<Department>(new DepartmentConfigurations());
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
