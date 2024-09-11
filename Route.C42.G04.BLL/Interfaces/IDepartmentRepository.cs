3149691112c04b0ec4d710583a8c0c3f63ea7175
entsarahmed <entesara500@gmail.com>
2024-09-11 17:50:16 +0300

    Layers Arch. Pattern - Business  Logic Layer

---
+++ Route.C42.G04.BLL/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,25 @@
+﻿using Route.C42.G04.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.C42.G04.BLL.Interfaces
+{
+    internal interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAll();
+
+        Department Get(int id);
+
+        int Add(Department entity);
+
+        int Update(Department entity);
+        int Delete(Department entity);
+
+
+
+
+    }
+}
---
+++ Route.C42.G04.BLL/Repositories/DepartmentRepository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Route.C42.G04.BLL.Interfaces;
+using Route.C42.G04.DAL.Data;
+using Route.C42.G04.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.C42.G04.BLL.Repositories
+{
+    internal class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly ApplicationDbContext _dbContext;//NULL
+
+        public DepartmentRepository( ApplicationDbContext dbContext)// Ask CLR for Creating Object from "ApplicationDbContext" 
+        {
+            _dbContext = dbContext; /*new ApplicationDbContext(new Microsoft.EntityFrameworkCore.DbContextOptions<ApplicationDbContext>());*/
+        }
+        public int Add(Department entity)
+        {
+          _dbContext.Departments.Add(entity);
+            return _dbContext.SaveChanges();
+        }
+        public int Update(Department entity)
+        {
+            _dbContext.Departments.Update(entity);
+            return _dbContext.SaveChanges();
+        }
+
+        public int Delete(Department entity)
+        {
+            _dbContext.Departments.Remove(entity);
+            return _dbContext.SaveChanges();
+
+        }
+
+        public Department Get(int id)
+        {
+           // return _dbContext.Departments.Find(id);//Find(new {StudentId = 10, CourseId = 1000});
+            return _dbContext.Find<Department>(id);// EF Core 3.1 New Feature
+
+
+            //var department =_dbContext.Departments.Local.Where(D => D.Id == id).FirstOrDefault();
+            //if (department == null)
+            //    department = _dbContext.Departments.Where(D => D.Id == id).FirstOrDefault();  
+            
+            //return department;
+        }
+
+        public IEnumerable<Department> GetAll()
+        
+        =>   _dbContext.Departments.AsNoTracking().ToList();
+        
+
+       
+    }
+}
